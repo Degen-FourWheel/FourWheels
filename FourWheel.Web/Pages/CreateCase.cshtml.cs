2688249df11b09c371c75cb418166f8b798ac839
Rene Rasmussen <rene069g@elevcampus.dk>
2023-03-22 13:51:51 +0100

    WIP

---
+++ FourWheel.Web/Pages/CreateCase.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FourWheel.Web.Pages
+{
+    public class CreateCaseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
